e773791c568ae69d6a32ea7205f1d7ae1e8b7b24
Brad Nelson <bjnelson@stanford.edu>
2015-10-12 12:40:33 -0700

    updated example

--- examples/JuliaSets.jl
+++ examples/JuliaSets.jl
@@ -1,12 +1,15 @@
 using JuliaSets
 
-Q(z) = z.^2
+Q(z) = z.^4 + 0.6 + 1im * 0.55
 
 xs = [linspace(-2, 2, 1000);]
 ys = copy(xs)
 
 A = escape_time(Q, xs, ys, 20, 100.0)
 
+# map to [0,2 \pi]
+A = atan(A)
+
 using Gadfly
 
-draw(SVG("z2.svg", 2inch, 2inch), spy(A))
+draw(PNG("example.png", 5cm, 5cm), spy(A))
